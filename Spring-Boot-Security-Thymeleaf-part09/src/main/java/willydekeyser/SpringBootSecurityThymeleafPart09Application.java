4cfeadb35ca99850b9ba5e3762970f539dc3b89c
Willy De Keyser <spring.boot.programming@gmail.com>
2026-01-29 17:20:43 +0100

    Setup

---
+++ Spring-Boot-Security-Thymeleaf-part09/src/main/java/willydekeyser/SpringBootSecurityThymeleafPart09Application.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityThymeleafPart09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityThymeleafPart09Application.class, args);
+	}
+
+}
